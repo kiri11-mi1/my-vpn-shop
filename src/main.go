c887f4b991dfa1b12b177ec0fed7459812610d8f
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-29 18:22:23 +0700

    add hello world bot

--- src/main.go
+++ src/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"os"
+)
 
+func main() {
+	bot, err := tg.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tg.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
+
+		msg := tg.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "I understand /sayhi and /status."
+		case "sayhi":
+			msg.Text = "Hi :)"
+		case "status":
+			msg.Text = "I'm ok."
+		default:
+			msg.Text = "I don't know that command"
+		}
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+
+	}
 }
