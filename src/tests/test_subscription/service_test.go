f4ac5fe510793c28eb8a4384b8ccd01d8645ffb6
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-31 18:43:53 +0700

    add tests

--- src/tests/test_subscription/service_test.go
+++ src/tests/test_subscription/service_test.go
@@ -2,6 +2,7 @@ package test_subscription
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"my-vpn-shop/subscription"
 	"testing"
 )
@@ -9,10 +10,30 @@ import (
 func TestService_GetActualPrice(t *testing.T) {
 	t.Run("get actual price", func(t *testing.T) {
 		const (
-			keysCount     = 6
-			totalVPNPrice = 300
-			expected      = 50.0
+			keysCount     int     = 6
+			totalVPNPrice float64 = 300
+			expected      float64 = 50.0
 		)
-		assert.Equal(t, expected, subscription.GetActualPrice(keysCount, totalVPNPrice))
+		actual, err := subscription.GetActualPrice(keysCount, totalVPNPrice)
+		require.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	})
+	t.Run("zero keys in vpn servers", func(t *testing.T) {
+		const (
+			keysCount     int     = 0
+			totalVPNPrice float64 = 300
+		)
+		_, err := subscription.GetActualPrice(keysCount, totalVPNPrice)
+		require.ErrorIs(t, err, subscription.ZeroKeysInServer)
+	})
+	t.Run("zero total price", func(t *testing.T) {
+		const (
+			keysCount     int     = 18
+			totalVPNPrice float64 = 0
+			expected      float64 = 0
+		)
+		actual, err := subscription.GetActualPrice(keysCount, totalVPNPrice)
+		require.NoError(t, err)
+		assert.Equal(t, expected, actual)
 	})
 }
