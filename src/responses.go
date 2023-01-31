7b07fc2c1f7fae1a13cc12c6c7f613cd05e057de
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-31 11:56:26 +0700

    add responses

--- src/responses.go
+++ src/responses.go
@@ -1,5 +1,11 @@
 package main
 
-const START string = "Привет! Тут ты можешь купить подписку на мой впн. Стоимость будет рассчитываться в зависимости от подключенных клиентов."
-const HELP string = "/buy"
-const NOT_KNOWN_COMMAND = "Не понимаю о чём ты говоришь"
+const START = "Привет! Тут ты можешь купить подписку на мой впн. Стоимость будет рассчитываться в зависимости от подключенных клиентов."
+const HELP = "/buy"
+const NotKnownCommand = "Не понимаю о чём ты говоришь"
+
+const InvoiceTitle = "Дай деняк"
+const InvoiceDescription = "Оплата подписки для доступа к VPN"
+const InvoiceCurrency = "rub"
+const InvoicePayload = "test"
+const StartParameter = "Оплати"
