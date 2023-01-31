807d243273111c1c218042d5aedaa581f02f5aea
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-01-31 18:43:48 +0700

    check devision by zero

--- src/subscription/service.go
+++ src/subscription/service.go
@@ -1,5 +1,8 @@
 package subscription
 
-func GetActualPrice(keysCount int, totalVPNPrice float64) float64 {
-	return totalVPNPrice / float64(keysCount)
+func GetActualPrice(keysCount int, totalVPNPrice float64) (float64, error) {
+	if keysCount == 0 {
+		return -1, ZeroKeysInServer
+	}
+	return totalVPNPrice / float64(keysCount), nil
 }
