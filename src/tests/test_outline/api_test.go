a93f6a0739302ff5f2b26da51f5aa03680581253
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-01 11:54:09 +0700

    add test for get keys

--- src/tests/test_outline/api_test.go
+++ src/tests/test_outline/api_test.go
@@ -1,1 +1,20 @@
 package test_outline
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"my-vpn-shop/outline"
+	"os"
+	"testing"
+)
+
+func TestApi_GetKeys(t *testing.T) {
+	t.Run("get keys from outline api", func(t *testing.T) {
+		var (
+			client = outline.NewOutlineClient(os.Getenv("VPN_URL_API"))
+		)
+		keys, err := client.GetKeys()
+		require.NoError(t, err)
+		assert.NotEmpty(t, keys)
+	})
+}
