cab97cb27d5b0a94a837de6e4ab9ee121c91086f
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-01 10:47:39 +0700

    add method get keys

---
+++ src/outline/api.go
@@ -0,0 +1,52 @@
+package outline
+
+import (
+	"encoding/json"
+	"github.com/valyala/fasthttp"
+	"net/http"
+)
+
+type AccessKey struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Password  string `json:"password"`
+	Port      string `json:"port"`
+	Method    string `json:"method"`
+	AccessUrl string `json:"accessUrl"`
+}
+
+type AccessKeys []AccessKey
+
+type KeysResponse struct {
+	Keys AccessKeys `json:"accessKeys"`
+}
+
+type OutlineClient struct {
+	ApiUrl string
+}
+
+func NewOutlineClient(apiUrl string) *OutlineClient {
+	return &OutlineClient{
+		ApiUrl: apiUrl,
+	}
+}
+
+func (o *OutlineClient) GetKeys() (AccessKeys, error) {
+	endpoint := "/access-keys"
+
+	req := &fasthttp.Request{}
+	res := &fasthttp.Response{}
+
+	req.Header.SetMethod(http.MethodGet)
+	req.SetRequestURI(o.ApiUrl + endpoint)
+
+	err := fasthttp.Do(req, res)
+	if err != nil {
+		return AccessKeys{}, err
+	}
+	keys := AccessKeys{}
+	if err := json.Unmarshal(res.Body(), &keys); err != nil {
+		return AccessKeys{}, err
+	}
+	return keys, nil
+}
