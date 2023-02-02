b32de3efb71455253eb21025502b9bb2bb86096b
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-02 10:47:46 +0700

    add check for negative total price and min valid value

--- src/subscription/service.go
+++ src/subscription/service.go
@@ -1,15 +1,23 @@
 package subscription
 
-import tg "gopkg.in/telebot.v3"
+import (
+	tg "gopkg.in/telebot.v3"
+)
+
+const MinAmount = 87.41 // valid minimal value for telegram payments
+const Label = "Актуальная цена за этот месяц"
 
 func GetActualPrice(keysCount int, totalVPNPrice float64) (tg.Price, error) {
 	if keysCount == 0 {
-		return tg.Price{}, ZeroKeysInServer
+		return tg.Price{}, ErrZeroKeysInServer
 	}
-	var (
-		tmp    = totalVPNPrice / float64(keysCount)
-		result = tg.Price{Label: "Актуальная цена за этот месяц", Amount: int(tmp * 100.00)}
-	)
+	if totalVPNPrice < 0 {
+		return tg.Price{}, ErrNegativeTotalPrice
+	}
+	tmp := totalVPNPrice / float64(keysCount)
+	if tmp < MinAmount {
+		return tg.Price{Label: Label, Amount: int(MinAmount * 100)}, nil
+	}
 
-	return result, nil
+	return tg.Price{Label: Label, Amount: int(tmp * 100.00)}, nil
 }
