98a5b0160146c13d34fc4376ce8ca865ae8a7636
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-03 12:39:36 +0700

    init constants

---
+++ src/subscription/constants.go
@@ -0,0 +1,7 @@
+package subscription
+
+const InvoiceTitle = "Пора платить за VPN"
+const InvoiceDescription = "Списание средств за использование VPN сервиса"
+const InvoiceCurrency = "RUB"
+const InvoicePayload = "invoice_vpn"
+const InvoiceImage = "https://cdn-icons-png.flaticon.com/512/6554/6554321.png"
