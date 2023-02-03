9d759ccd39d0e046ec0b3c7287767281fba004f0
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-03 14:01:25 +0700

    add get invoice func

--- src/subscription/service.go
+++ src/subscription/service.go
@@ -4,7 +4,7 @@ import (
 	tg "gopkg.in/telebot.v3"
 )
 
-const MinAmount = 87.41 // valid minimal value for telegram payments
+const MinAmount = 60.00 // valid minimal value for telegram payments
 const Label = "Актуальная цена за этот месяц"
 
 func GetActualPrice(keysCount int, totalVPNPrice float64) (tg.Price, error) {
@@ -20,4 +20,22 @@ func GetActualPrice(keysCount int, totalVPNPrice float64) (tg.Price, error) {
 	}
 
 	return tg.Price{Label: Label, Amount: int(tmp * 100.00)}, nil
+}
+
+func GetInvoice(keysCount int, totalVpnPrice float64, providerToken string) (tg.Invoice, error) {
+	price, err := GetActualPrice(keysCount, totalVpnPrice)
+	if err != nil {
+		return tg.Invoice{}, err
+	}
+	file := tg.File{FileURL: InvoiceImage}
+	invoice := tg.Invoice{
+		Title:       InvoiceTitle,
+		Description: InvoiceDescription,
+		Payload:     InvoicePayload,
+		Currency:    InvoiceCurrency,
+		Token:       providerToken,
+		Prices:      []tg.Price{price},
+		Photo:       &tg.Photo{File: file},
+	}
+	return invoice, nil
 }
