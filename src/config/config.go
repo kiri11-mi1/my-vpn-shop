6e8f360f0610597fb75086d8feac27ef222e55ee
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-03 13:08:20 +0700

    add config

--- src/config/config.go
+++ src/config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"github.com/caarlos0/env/v6"
+	"log"
+)
+
+type Config struct {
+	ApiUrl        string  `env:"VPN_URL_API"`
+	TotalVpnPrice float64 `env:"TOTAL_VPN_PRICE"`
+	TelegramToken string  `env:"TELEGRAM_TOKEN"`
+	AdminChatId   string  `env:"ADMIN_CHAT_ID"`
+	ProviderToken string  `env:"PROVIDER_TOKEN"`
+}
+
+var cfg Config
+
+func Get() Config {
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalln("Config", err)
+	}
+	return cfg
+}
