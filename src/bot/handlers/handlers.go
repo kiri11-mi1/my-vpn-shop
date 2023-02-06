5160d52466cdd9bd4cb5c6cb5a1b41779088f30c
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-06 14:57:53 +0700

    add handler constructor

---
+++ src/bot/handlers/handlers.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"my-vpn-shop/db"
+	"my-vpn-shop/outline"
+)
+
+type Storage interface {
+	GetSubscribers() ([]db.Subscriber, error)
+	InsertSubscriber(id int64, name string) (db.Subscriber, error)
+	InsertAccessKey(id, name, accessUrl string, sub db.Subscriber) (db.AccessKey, error)
+	DeleteSubscriber(id int64) error
+	GetKeyBySubId(id int64) (db.AccessKey, error)
+	InsertPayment(amount float64, sub db.Subscriber) (db.Payment, error)
+}
+
+type API interface {
+	GetKeys() (outline.AccessKeys, error)
+	CreateKey() (outline.AccessKey, error)
+	ChangeKeyName(name string, key outline.AccessKey) error
+	DeleteKey(key outline.AccessKey) error
+	GetAccess(name string) (outline.AccessKey, error)
+}
+
+type Handlers struct {
+	storage Storage
+	api     API
+}
+
+func NewHandlers(storage Storage, api API) *Handlers {
+	return &Handlers{storage: storage, api: api}
+}
