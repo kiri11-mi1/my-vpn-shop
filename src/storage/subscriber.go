5bde67230a9e80ab08f076b3b1699f6664223995
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-06 16:18:28 +0700

    change methods

--- src/storage/subscriber.go
+++ src/storage/subscriber.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"my-vpn-shop/db"
+	"time"
 )
 
 func (p *Postgres) GetSubscribers() ([]db.Subscriber, error) {
@@ -13,7 +14,7 @@ func (p *Postgres) GetSubscribers() ([]db.Subscriber, error) {
 	subs := []db.Subscriber{}
 	for rows.Next() {
 		s := db.Subscriber{}
-		if err := rows.Scan(&s.ID, &s.Name); err != nil {
+		if err := rows.Scan(&s.ID, &s.Name, &s.PayedAt); err != nil {
 			return nil, err
 		}
 		subs = append(subs, s)
@@ -22,9 +23,10 @@ func (p *Postgres) GetSubscribers() ([]db.Subscriber, error) {
 }
 
 func (p *Postgres) InsertSubscriber(id int64, name string) (db.Subscriber, error) {
+	payedAt := time.Now()
 	_, err := p.db.Exec(
-		"insert into subscriber (id, name) values ($1, $2)",
-		id, name,
+		"insert into subscriber (id, name, payed_at) values ($1, $2, $3)",
+		id, name, payedAt.Format("2006-01-02"),
 	)
 	if err != nil {
 		return db.Subscriber{}, err
