7e344964c5702715144eb1b729f2a0f619de9061
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-06 14:58:05 +0700

    change main script

--- src/bot/main.go
+++ src/bot/main.go
@@ -1,33 +1,57 @@
 package main
 
 import (
+	"fmt"
+	_ "github.com/lib/pq"
 	tg "gopkg.in/telebot.v3"
 	"log"
 	"my-vpn-shop/bot/handlers"
 	"my-vpn-shop/config"
+	"my-vpn-shop/db"
+	"my-vpn-shop/outline"
+	"my-vpn-shop/storage"
 	"time"
 )
 
 func main() {
-
 	pref := tg.Settings{
 		Token:  config.Get().TelegramToken,
 		Poller: &tg.LongPoller{Timeout: 10 * time.Second},
 	}
-
 	b, err := tg.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+
+	connStr := fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s sslmode=disable",
+		config.Get().PostgresUser,
+		config.Get().PostgresPassword,
+		config.Get().PostgresNameDatabase,
+		config.Get().PostgresHost,
+	)
+	pgClient, err := db.Connect("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	pgDB := pgClient.Client()
+	pgStorage := storage.NewPostgresDB(pgDB)
+
+	outlineAPI := outline.NewOutlineClient(config.Get().ApiUrl)
+
+	handlerManager := handlers.NewHandlers(pgStorage, outlineAPI)
+
 	log.Println(b.Me.Username, "start working...")
 
 	b.Handle("/start", handlers.HandleStart)
-	b.Handle("/vpn", handlers.HandleVPN)
-	b.Handle(tg.OnCheckout, func(c tg.Context) error {
-		return c.Accept()
-	})
+	b.Handle("/vpn", handlerManager.HandleVPN)
+	b.Handle(&handlers.ButtonDisconnect, handlerManager.HandleDisconnect)
+	b.Handle(&handlers.ButtonLostKey, handlerManager.HandleLostKey)
+	b.Handle(tg.OnPayment, handlerManager.HandleSuccessPayment)
+	b.Handle(tg.OnCheckout, handlerManager.HandleAcceptPayment)
 	b.Handle("/help", handlers.HandleHelp)
-	b.Handle("/connections", handlers.HandleConnections)
+	b.Handle("/connections", handlerManager.HandleConnections)
 	b.Start()
 }
