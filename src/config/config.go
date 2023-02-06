9e65fcf43bd42c5d8ab52500c8cab41bb34ef787
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-06 15:00:21 +0700

    add config structures

--- src/config/config.go
+++ src/config/config.go
@@ -6,11 +6,15 @@ import (
 )
 
 type Config struct {
-	ApiUrl        string  `env:"VPN_URL_API"`
-	TotalVpnPrice float64 `env:"TOTAL_VPN_PRICE"`
-	TelegramToken string  `env:"TELEGRAM_TOKEN"`
-	AdminChatId   string  `env:"ADMIN_CHAT_ID"`
-	ProviderToken string  `env:"PROVIDER_TOKEN"`
+	ApiUrl               string  `env:"VPN_URL_API"`
+	TotalVpnPrice        float64 `env:"TOTAL_VPN_PRICE"`
+	TelegramToken        string  `env:"TELEGRAM_TOKEN"`
+	AdminChatId          string  `env:"ADMIN_CHAT_ID"`
+	ProviderToken        string  `env:"PROVIDER_TOKEN"`
+	PostgresUser         string  `env:"POSTGRES_USER"`
+	PostgresPassword     string  `env:"POSTGRES_PASSWORD"`
+	PostgresNameDatabase string  `env:"POSTGRES_DB"`
+	PostgresHost         string  `env:"POSTGRES_HOST"`
 }
 
 var cfg Config
