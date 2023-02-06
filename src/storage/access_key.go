c51a288db171a0e2e40d60c51b5996d87a11110e
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-06 15:00:54 +0700

    access keys methods

--- src/storage/access_key.go
+++ src/storage/access_key.go
@@ -1,1 +1,29 @@
 package storage
+
+import "my-vpn-shop/db"
+
+func (p *Postgres) InsertAccessKey(id, name, accessUrl string, sub db.Subscriber) (db.AccessKey, error) {
+	_, err := p.db.Exec(
+		"insert into accesskey (id, name, access_url, subscriber_id) values ($1, $2, $3, $4)",
+		id, name, accessUrl, sub.ID,
+	)
+	if err != nil {
+		return db.AccessKey{}, err
+	}
+	key := db.AccessKey{ID: id, Name: name, AccessUrl: accessUrl, Subscriber: sub}
+	return key, nil
+}
+
+func (p *Postgres) GetKeyBySubId(id int64) (db.AccessKey, error) {
+	rows, err := p.db.Query("select * from accesskey where subscriber_id=$1", id)
+	if err != nil {
+		return db.AccessKey{}, err
+	}
+	rows.Next()
+	defer rows.Close()
+	key := db.AccessKey{}
+	if err := rows.Scan(&key.ID, &key.Name, &key.AccessUrl, &key.Subscriber.ID); err != nil {
+		return db.AccessKey{}, err
+	}
+	return key, nil
+}
