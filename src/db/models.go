5b20e26c438ffd53e6bd7bae80d18cf2a62faf01
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-06 16:18:39 +0700

    delete payment model

--- src/db/models.go
+++ src/db/models.go
@@ -3,14 +3,9 @@ package db
 import "time"
 
 type Subscriber struct {
-	ID   int64
-	Name string
-}
-
-type Payment struct {
-	CreatedAt  time.Time
-	Amount     float64
-	Subscriber Subscriber
+	ID      int64
+	Name    string
+	PayedAt time.Time
 }
 
 type AccessKey struct {
