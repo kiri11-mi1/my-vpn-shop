daaafa3b5495d0d467d762b7fff3ea208d4f2af3
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-07 11:50:17 +0700

    test get access

--- src/tests/test_outline/api_test.go
+++ src/tests/test_outline/api_test.go
@@ -3,15 +3,15 @@ package test_outline
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"my-vpn-shop/config"
 	"my-vpn-shop/outline"
-	"os"
 	"testing"
 )
 
 func TestApi_GetKeys(t *testing.T) {
 	t.Run("get keys from outline api", func(t *testing.T) {
 		var (
-			client = outline.NewOutlineClient(os.Getenv("VPN_URL_API"))
+			client = outline.NewOutlineClient(config.Get().ApiUrl)
 		)
 		keys, err := client.GetKeys()
 		require.NoError(t, err)
@@ -40,7 +40,7 @@ func TestApi_GetKeys(t *testing.T) {
 func TestApi_CreateKey_DeleteKey(t *testing.T) {
 	t.Run("create key via outline api and delete it", func(t *testing.T) {
 		var (
-			client = outline.NewOutlineClient(os.Getenv("VPN_URL_API"))
+			client = outline.NewOutlineClient(config.Get().ApiUrl)
 		)
 		key, err := client.CreateKey()
 		require.NoError(t, err)
@@ -49,7 +49,7 @@ func TestApi_CreateKey_DeleteKey(t *testing.T) {
 	})
 	t.Run("delete not existing key", func(t *testing.T) {
 		var (
-			client = outline.NewOutlineClient(os.Getenv("VPN_URL_API"))
+			client = outline.NewOutlineClient(config.Get().ApiUrl)
 		)
 		key, err := client.CreateKey()
 		require.NoError(t, err)
@@ -64,7 +64,7 @@ func TestApi_CreateKey_DeleteKey(t *testing.T) {
 func TestApi_ChangeKeyName(t *testing.T) {
 	t.Run("change key name", func(t *testing.T) {
 		var (
-			client = outline.NewOutlineClient(os.Getenv("VPN_URL_API"))
+			client = outline.NewOutlineClient(config.Get().ApiUrl)
 		)
 		key, err := client.CreateKey()
 		require.NoError(t, err)
@@ -76,7 +76,7 @@ func TestApi_ChangeKeyName(t *testing.T) {
 	})
 	t.Run("empty key name", func(t *testing.T) {
 		var (
-			client = outline.NewOutlineClient(os.Getenv("VPN_URL_API"))
+			client = outline.NewOutlineClient(config.Get().ApiUrl)
 		)
 		key, err := client.CreateKey()
 		require.NoError(t, err)
@@ -88,7 +88,7 @@ func TestApi_ChangeKeyName(t *testing.T) {
 	})
 	t.Run("not existing key", func(t *testing.T) {
 		var (
-			client = outline.NewOutlineClient(os.Getenv("VPN_URL_API"))
+			client = outline.NewOutlineClient(config.Get().ApiUrl)
 		)
 		key, err := client.CreateKey()
 		require.NoError(t, err)
@@ -96,5 +96,18 @@ func TestApi_ChangeKeyName(t *testing.T) {
 		require.NoError(t, client.DeleteKey(key))
 
 		require.ErrorIs(t, client.ChangeKeyName("test key", key), outline.ErrInApi)
+	})
+}
+
+func TestApi_GetAccess(t *testing.T) {
+	t.Run("create access key with name", func(t *testing.T) {
+		var (
+			client = outline.NewOutlineClient(config.Get().ApiUrl)
+			name   = "test key outline"
+		)
+		key, err := client.GetAccess(name)
+		require.NoError(t, err)
+		assert.NotEmpty(t, key)
+		require.NoError(t, client.DeleteKey(key))
 	})
 }
