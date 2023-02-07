1ea38ce44ee37f2bb9d2f272ca64a69df199e8f7
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-07 11:43:32 +0700

    add connections handler

--- src/bot/handlers/connections.go
+++ src/bot/handlers/connections.go
@@ -6,6 +6,10 @@ import (
 )
 
 func (h *Handlers) HandleConnections(c tg.Context) error {
-	message := fmt.Sprintf("В разработке")
+	subs, err := h.storage.GetSubscribers()
+	if err != nil {
+		return nil
+	}
+	message := fmt.Sprintf("Количество подключений на данный момент: %d", len(subs))
 	return c.Send(message)
 }
