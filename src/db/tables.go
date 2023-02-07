9f48cfac153a2ee5e4bed8f3765fd7539acda6e9
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-07 16:53:42 +0700

    go module with query creatng tables

--- src/db/tables.go
+++ src/db/tables.go
@@ -1,15 +1,15 @@
 package db
 
 var QUERY = `
-	create table if not exists Subscriber(
+create table if not exists Subscriber(
 		id integer primary key,
 		name varchar(128),
 		payed_at date
-	);
-	create table if not exists AccessKey(
+);
+create table if not exists AccessKey(
 		id varchar(128) primary key,
 		name varchar(128),
 		access_url varchar(256),
-		subscriber_id integer references Subscriber(id) on delete cascade
-	);
+		subscriber_id integer references subscriber(id) on delete cascade
+);
 `
