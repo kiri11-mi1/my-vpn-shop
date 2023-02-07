5936f61ac95cd34287ea95e330f280383751215d
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-07 23:40:46 +0700

    add tests for delete subs

--- src/tests/test_storage/subscriber_test.go
+++ src/tests/test_storage/subscriber_test.go
@@ -58,3 +58,51 @@ func TestSubscriber_InsertSubscriber(t *testing.T) {
 		assert.IsType(t, time.Now(), actual.PayedAt)
 	})
 }
+
+func TestSubscriber_DeleteSubscriber(t *testing.T) {
+	t.Run("delete subscriber from db", func(t *testing.T) {
+		dbName := fmt.Sprintf("test_store_%s.db", uuid.New())
+		sqliteClient, err := db.Connect("sqlite3", dbName)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		sqliteDB := sqliteClient.Client()
+		require.NoError(t, sqliteClient.CreateTables())
+		sqliteStorage := storage.NewSQlDB(sqliteDB)
+		var (
+			subID   int64 = 123
+			subName       = "test user"
+		)
+
+		_, err = sqliteStorage.InsertSubscriber(subID, subName)
+		require.NoError(t, err)
+		require.NoError(t, sqliteStorage.DeleteSubscriber(subID))
+		subs, err := sqliteStorage.GetSubscribers()
+		require.NoError(t, err)
+		assert.Zero(t, len(subs))
+	})
+	t.Run("delete not existing subscriber from db", func(t *testing.T) {
+		dbName := fmt.Sprintf("test_store_%s.db", uuid.New())
+		sqliteClient, err := db.Connect("sqlite3", dbName)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		sqliteDB := sqliteClient.Client()
+		require.NoError(t, sqliteClient.CreateTables())
+		sqliteStorage := storage.NewSQlDB(sqliteDB)
+		var (
+			notExistingID int64 = 123
+			lenSubArray         = 10
+		)
+		for i := 1; i <= lenSubArray; i++ {
+			_, err = sqliteStorage.InsertSubscriber(int64(i), fmt.Sprintf("test sub %d", i))
+			require.NoError(t, err)
+		}
+		require.NoError(t, sqliteStorage.DeleteSubscriber(notExistingID))
+		subs, err := sqliteStorage.GetSubscribers()
+		require.NoError(t, err)
+		assert.Equal(t, lenSubArray, len(subs))
+	})
+}
