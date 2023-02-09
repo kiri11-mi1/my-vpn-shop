bd0efc5d6eb293512d52b3c87927f0212006d117
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-09 14:25:59 +0700

    add utils tests

---
+++ src/tests/test_subscription/utils_test.go
@@ -0,0 +1,44 @@
+package test_subscription
+
+import (
+	"github.com/stretchr/testify/assert"
+	"my-vpn-shop/subscription"
+	"testing"
+	"time"
+)
+
+func TestUtils_IsTimeOutPay(t *testing.T) {
+	t.Run("test time out", func(t *testing.T) {
+		var (
+			inputLastPayDate = time.Date(2023, 01, 12, 0, 0, 0, 0, nil)
+			inputCurrentTime = time.Date(2023, 02, 15, 0, 0, 0, 0, nil)
+		)
+		actual := subscription.IsTimeOutPay(inputLastPayDate, inputCurrentTime)
+		assert.True(t, actual)
+	})
+}
+
+func TestUtils_GetActualPrice(t *testing.T) {
+
+}
+
+func TestUtils_GetName(t *testing.T) {
+	t.Run("get name sub", func(t *testing.T) {
+		var (
+			username       = "test"
+			id       int64 = 245
+			expected       = "sub_test_245"
+		)
+		actual := subscription.GetName(username, id)
+		assert.Equal(t, expected, actual)
+	})
+	t.Run("get name with empty username", func(t *testing.T) {
+		var (
+			username       = ""
+			id       int64 = 245
+			expected       = "sub__245"
+		)
+		actual := subscription.GetName(username, id)
+		assert.Equal(t, expected, actual)
+	})
+}
