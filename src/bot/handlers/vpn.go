3b780d4860db2f076fc49358675995733d23e35a
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-09 14:27:57 +0700

    change logic from db methods to service methods

--- src/bot/handlers/vpn.go
+++ src/bot/handlers/vpn.go
@@ -3,10 +3,6 @@ package handlers
 import (
 	"fmt"
 	tg "gopkg.in/telebot.v3"
-	"log"
-	"my-vpn-shop/config"
-	"my-vpn-shop/outline"
-	"my-vpn-shop/subscription"
 )
 
 var (
@@ -15,28 +11,24 @@ var (
 )
 
 func (h *Handlers) HandleVPN(c tg.Context) error {
-	subs, err := h.storage.GetSubscribers()
+	if h.service.IsConnected(c.Chat().ID) {
+		buttons := &tg.ReplyMarkup{}
+		message := "Вы уже подключены к VPN."
+		buttons.Inline(buttons.Row(ButtonDisconnect, ButtonLostKey))
+		return c.Send(message, buttons)
+	}
+
+	message := "Чтобы пользоваться VPN нужно сразу заплатить деньгу за месяц. Бот сам пришлёт следующий счёт оплаты."
+	_, err := c.Bot().Send(c.Recipient(), message)
 	if err != nil {
 		return err
 	}
-
-	if !subscription.IsConnected(subs, c.Chat().ID) {
-		message := "Чтобы пользоваться VPN нужно сразу заплатить деньгу за месяц. Бот сам пришлёт следующий счёт оплаты."
-		_, err := c.Bot().Send(c.Recipient(), message)
-		if err != nil {
-			return err
-		}
-		invoice, err := subscription.GetInvoice(len(subs)+1, config.Get().TotalVpnPrice, config.Get().ProviderToken)
-		if err != nil {
-			return err
-		}
-		_, err = invoice.Send(c.Bot(), c.Recipient(), nil)
+	invoice, err := h.service.GetInvoice()
+	if err != nil {
 		return err
 	}
-	buttons := &tg.ReplyMarkup{}
-	message := "Вы уже подключены к VPN."
-	buttons.Inline(buttons.Row(ButtonDisconnect, ButtonLostKey))
-	return c.Send(message, buttons)
+	_, err = invoice.Send(c.Bot(), c.Recipient(), nil)
+	return err
 }
 
 func (h *Handlers) HandleAcceptPayment(c tg.Context) error {
@@ -44,41 +36,26 @@ func (h *Handlers) HandleAcceptPayment(c tg.Context) error {
 }
 
 func (h *Handlers) HandleSuccessPayment(c tg.Context) error {
-	newSub, err := h.storage.InsertSubscriber(c.Chat().ID, c.Sender().Username)
+	//if h.service.IsConnected(c.Chat().ID) {
+	//	// продление подписки
+	//}
+	key, err := h.service.Connect(c.Chat().ID, c.Sender().Username)
 	if err != nil {
 		return err
 	}
-	log.Println("Add sub: ", newSub.ID, newSub.Name)
-	createdKey, err := h.api.GetAccess(subscription.GetName(newSub.Name, newSub.ID))
-	if err != nil {
-		return err
-	}
-	log.Println("Create access key", createdKey.Id, createdKey.Name, "for sub", newSub.Name, newSub.ID)
-	dbKey, err := h.storage.InsertAccessKey(createdKey.Id, createdKey.Name, createdKey.AccessUrl, newSub)
-	if err != nil {
-		return err
-	}
-	log.Println("Add key in db:", dbKey.Name, dbKey.ID, dbKey.Subscriber.Name)
-	message := fmt.Sprintf("Ключ доступа для Outline:\n`%s`", dbKey.AccessUrl)
+	message := fmt.Sprintf("Ключ доступа для Outline:\n`%s`", key.AccessUrl)
 	return c.Send(message, tg.ModeMarkdown)
 }
 
 func (h *Handlers) HandleDisconnect(c tg.Context) error {
-	key, err := h.storage.GetKeyBySubId(c.Sender().ID)
-	if err != nil {
-		return err
-	}
-	if err := h.api.DeleteKey(outline.AccessKey{Id: key.ID}); err != nil {
-		return err
-	}
-	if err := h.storage.DeleteSubscriber(c.Sender().ID); err != nil {
+	if err := h.service.Disconnect(c.Chat().ID); err != nil {
 		return err
 	}
 	return c.Send("Вы отключены от совместного использования VPN")
 }
 
 func (h *Handlers) HandleLostKey(c tg.Context) error {
-	key, err := h.storage.GetKeyBySubId(c.Sender().ID)
+	key, err := h.service.FindKey(c.Chat().ID)
 	if err != nil {
 		return err
 	}
