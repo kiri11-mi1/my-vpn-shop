35f41e87d85f1998bf5a211cb4dd02a30b81ca8a
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-10 00:29:03 +0700

    delete get invoice from interface

--- src/bot/handlers/handlers.go
+++ src/bot/handlers/handlers.go
@@ -1,14 +1,12 @@
 package handlers
 
 import (
-	tg "gopkg.in/telebot.v3"
 	"my-vpn-shop/db"
 )
 
 type SubscriptionService interface {
 	IsConnected(chatId int64) bool
 	Connect(chatID int64, username string) (db.AccessKey, error)
-	GetInvoice() (tg.Invoice, error)
 	Disconnect(chatId int64) error
 	FindKey(chatID int64) (db.AccessKey, error)
 	GetCountSubs() (int, error)
