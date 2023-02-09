7e5891de5c5c72b3847ae076f48706d3d574e93c
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-09 14:25:51 +0700

    add utils functions

---
+++ src/subscription/utils.go
@@ -0,0 +1,49 @@
+package subscription
+
+import (
+	"fmt"
+	tg "gopkg.in/telebot.v3"
+	"log"
+	"time"
+)
+
+func GetActualPrice(keysCount int, totalVPNPrice float64) (tg.Price, error) {
+	if keysCount == 0 {
+		return tg.Price{}, ErrZeroKeysInServer
+	}
+	if totalVPNPrice < 0 {
+		return tg.Price{}, ErrNegativeTotalPrice
+	}
+	tmp := totalVPNPrice / float64(keysCount)
+	if tmp < MinAmount {
+		return tg.Price{Label: Label, Amount: int(MinAmount * 100)}, nil
+	}
+
+	return tg.Price{Label: Label, Amount: int(tmp * 100.00)}, nil
+}
+
+func GetName(sub string, id int64) string {
+	return fmt.Sprintf("sub_%s_%d", sub, id)
+}
+
+func IsPayDay(lastPayTime, currentTime time.Time) bool {
+	nextPayTime := lastPayTime.AddDate(0, 1, 0)
+	if nextPayTime.Month() == time.Now().Month() && nextPayTime.Day() == time.Now().Day() {
+		return true
+	}
+	return false
+}
+
+func IsTimeOutPay(lastPayTime, currentTime time.Time) bool {
+	nextPayTime := lastPayTime.AddDate(0, 1, 0)
+	if nextPayTime.After(time.Now()) {
+		return true
+	}
+	return false
+}
+
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
