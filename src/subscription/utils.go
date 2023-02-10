70ec12598f4fd6a7c3b820b445a439eeabb884a1
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-10 10:49:54 +0700

    refactor

--- src/subscription/utils.go
+++ src/subscription/utils.go
@@ -7,14 +7,14 @@ import (
 	"time"
 )
 
-func GetActualPrice(keysCount int, totalVPNPrice float64) (tg.Price, error) {
-	if keysCount == 0 {
+func GetActualPrice(subCount int, totalVPNPrice float64) (tg.Price, error) {
+	if subCount == 0 {
 		return tg.Price{}, ErrZeroKeysInServer
 	}
 	if totalVPNPrice < 0 {
 		return tg.Price{}, ErrNegativeTotalPrice
 	}
-	tmp := totalVPNPrice / float64(keysCount)
+	tmp := totalVPNPrice / float64(subCount)
 	if tmp < MinAmount {
 		return tg.Price{Label: Label, Amount: int(MinAmount * 100)}, nil
 	}
