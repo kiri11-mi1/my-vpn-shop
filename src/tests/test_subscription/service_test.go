ff805ad6bed53b3c31f01838f87374c4fedb34fc
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-02-10 11:54:50 +0700

    add test connect

--- src/tests/test_subscription/service_test.go
+++ src/tests/test_subscription/service_test.go
@@ -1,13 +1,55 @@
 package test_subscription
 
 import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"log"
+	"my-vpn-shop/config"
+	"my-vpn-shop/db"
+	"my-vpn-shop/outline"
+	"my-vpn-shop/storage"
+	"my-vpn-shop/subscription"
 	"testing"
 )
 
-func TestService_GetInvoice(t *testing.T) {
+func TestService_Connect(t *testing.T) {
+	t.Run("sub is connected", func(t *testing.T) {
+		dbName := fmt.Sprintf("test_store_%s.db", uuid.New())
+		sqliteClient, err := db.Connect("sqlite3", dbName)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		sqliteDB := sqliteClient.Client()
+		require.NoError(t, sqliteClient.CreateTables())
+		sqliteStorage := storage.NewSQlDB(sqliteDB)
+		api := outline.NewOutlineClient(config.Get().ApiUrl)
+		service := subscription.NewSubscriptionService(sqliteStorage, api)
 
-}
+		var (
+			subId             int64 = 123
+			subName                 = "test user"
+			expectedCountSubs       = 1
+		)
+		expectedKey, err := service.Connect(subId, subName)
+		require.NoError(t, err)
+		assert.NotEmpty(t, expectedKey)
 
-func TestService_IsConnected(t *testing.T) {
+		actualSubs, err := sqliteStorage.GetSubscribers()
+		require.NoError(t, err)
+		assert.Equal(t, expectedCountSubs, len(actualSubs))
 
+		sub, err := sqliteStorage.GetSubByID(subId)
+		require.NoError(t, err)
+		assert.Equal(t, subName, sub.Name)
+		assert.Equal(t, subId, sub.ID)
+
+		actualKey, err := sqliteStorage.GetKeyBySubId(subId)
+		require.NoError(t, err)
+		assert.Equal(t, expectedKey, actualKey)
+
+		require.NoError(t, api.DeleteKey(outline.AccessKey{Id: actualKey.ID}))
+	})
 }
